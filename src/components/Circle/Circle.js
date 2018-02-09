1ee673339bb91872ad7909a0a05e936fee38fd7d
Paul Dubovsky <pauldubovsky@Pauls-MacBook-Air.local>
2018-02-09 15:12:04 -0800

    second commit

--- src/components/Circle/Circle.js
+++ src/components/Circle/Circle.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const circle = () => {
+
+    const style = {
+        backgroundColor: 'green'
+    };
+
     return (
-        <div className="circles">
+        <div className="circles" style={style}>
 
         </div>
     )
